3b2266472caa97d9769d811089d424cd027ec9e5
nathanael-joseph <nyebgui66@gmail.com>
2017-07-10 14:15:17 +0300

    updated comicbook model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -14,5 +14,24 @@ namespace ComicBookGallery.Models
         public Artist[] Artists { get; set; }
         public bool Favourite { get; set; }
 
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        // series-title-issuenumber.jpg
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+
+            }
+        }
+
     }
 }
